719ca17348dff6af1604d6178fd0494fdfe05c77
carlo <cakhill88@gmail.com>
2019-11-19 17:12:48 +0000

    Nov 19 End of Day

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
+import AppliedRoute from "./components/AppliedRoute";
+import NewNote from "./containers/NewNote";
 
-export default function Routes() {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      { /* Finally, catch all unmatched routes */ }
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+
+export default function Routes({ appProps }) {
+    return (
+      <Switch>
+        <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+        <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+        <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+        { /* Finally, catch all unmatched routes */ }
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
